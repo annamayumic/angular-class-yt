f45417feb279bb4a7dd86cc4efbaffcd6f8bcbfa
Anna Mayumi <carvalhoannamayumi@gmail.com>
2024-08-01 13:57:49 +0900

    aula 8, 9 10

---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.css'
+})
+export class EventosComponent implements OnInit{
+  show: boolean = true
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  showMessage():void {
+    this.show = !this.show
+  }
+
+
+}
