fe6dc40970e530762c0c3fa63cca9a62ab3ed9e3
Anna Mayumi <carvalhoannamayumi@gmail.com>
2024-08-01 16:43:15 +0900

    aula 17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'if-render', component:IfRenderComponent},
+  {path: 'emitter', component: EmitterComponent},
+  {path: 'exibirMSG', component: EventosComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
